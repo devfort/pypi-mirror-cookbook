f8d4f74b7349a74f400e3eff0951118083032770
Steve Marshall <steve@nascentguruism.com>
2013-10-27 21:22:33 +0000

    Attributise bandersnatch conf.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default['pypi_mirror']['user'] = 'fort'
 default['pypi_mirror']['data_dir'] = '/data/pypi'
+default['pypi_mirror']['remote_pypi'] = 'https://pypi.python.org'
 
 default['pypi_mirror']['apache']['listen_hostname'] = '*'
 default['pypi_mirror']['apache']['listen_port'] = '80'

ebd4a87a6c6c60bdea6e196229a56fe2acc7eb4f
Steve Marshall <steve@nascentguruism.com>
2013-11-04 19:26:41 +0000

    Simplify using mirror-core.

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,37 +6,18 @@ python_pip "bandersnatch" do
   action :install
 end
 
-directory node.pypi_mirror.data_dir do
+template "/etc/bandersnatch.conf" do
+  source "bandersnatch.conf.erb"
   owner node.pypi_mirror.user
   group node.pypi_mirror.user
-  mode "0755"
-  recursive true
+  mode "0644"
 end
 
-{
-  "bandersnatch.conf.erb" => "/etc/bandersnatch.conf",
-  "services/pypi-mirror.conf.erb"      => "/etc/init/pypi-mirror.conf",
-  "services/pypi-mirror-shim.conf.erb" => "/etc/init/pypi-mirror-shim.conf",
-}.each do |src, target|
-  template target do
-    source src
-    owner node.pypi_mirror.user
-    group node.pypi_mirror.user
-    mode "0644"
-  end
-end
-
-service "pypi-mirror-shim" do
-  provider Chef::Provider::Service::Upstart
-  action :restart
-end
-
-# Serve the gems (when they finish mirroring)
-web_app "pypi_mirror" do
-  docroot "#{node.pypi_mirror.data_dir}/web"
+mirror "pypi" do
+  target node.pypi_mirror.data_dir
+  user node.pypi_mirror.user
   hostname node.pypi_mirror.apache.listen_hostname
   port node.pypi_mirror.apache.listen_port
+  command "bandersnatch mirror"
+  cookbook "mirror-core"
 end
-
-
-log "Started mirroring pypi; tail /var/log/upstart/pypi-mirror.log to monitor."

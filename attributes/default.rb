f085d30832412afd88525f35d92f49043ffe9bbf
Steve Marshall <steve@nascentguruism.com>
2013-11-06 16:35:22 +0000

    Attributify remote host.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['pypi_mirror']['user'] = 'fort'
 default['pypi_mirror']['data_dir'] = '/data/pypi'
-default['pypi_mirror']['remote_pypi'] = 'https://pypi.python.org'
+default['pypi_mirror']['remote'] = 'https://pypi.python.org'
 
 default['pypi_mirror']['apache']['listen_hostname'] = 'pypi.fort'
 default['pypi_mirror']['apache']['listen_port'] = '80'
